c70ed9c5ba1abb41b82957dedc6d086c2144c30f
Kavinsky <nmunozfernandez@gmail.com>
2011-09-16 14:59:01 +0200

    added phpdoc comments to Queue class and comments for config file

--- config/queue.php
+++ config/queue.php
@@ -13,10 +13,28 @@
 return array(
 
 	
+	/*
+	 * Current active driver (i.e: db = Driver_DB)
+	 */
 	'activedriver' => 'db',
+	
+	/*
+	 * list of drivers and their configurations
+	 */
 	'drivers' => array(
 		//
 		// DB Driver
+		/* Database table for DB Driver
+		 * ===================================================================================
+		  CREATE TABLE `queue` (
+			  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+			  `queue` varchar(40) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+			  `priority` int(1) NOT NULL DEFAULT '5',
+			  `payload` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+		  =================================================================================== 
+		*/
 		'db' => array(
 			'drivername' 	=> 'Driver_DB',
 			'tablename'		=> 'queue',
@@ -30,11 +48,13 @@ return array(
 			'store'			=> 'queue',
 		),
 		
+		// not working
 		'beanstalkd' => array(
 			'drivername'	=> 'Driver_Beanstalkd',
 			
 		),
 		
+		//not working
 		'mongodb'	=> array(
 			'drivername'	=> 'Driver_MongoDB',
 			
