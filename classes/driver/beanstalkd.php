ac372c26ca38de5097a606a4197e3861f77e62bb
Kavinsky <nmunozfernandez@gmail.com>
2011-09-16 16:16:21 +0300

    cleaned all code of Driver_Beanstalkd methods

--- classes/driver/beanstalkd.php
+++ classes/driver/beanstalkd.php
@@ -31,53 +31,31 @@ class Driver_Beanstalkd
 	
 	public function push($queue, $item)
 	{
-		$this->
+		
 	}
 	
 	public function pop($queue)
 	{
-		$item = $this->_redis->lpop('queue:' . $queue);
-		if(!$item)
-		{
-			return;
-		}
 		
-		return json_decode($item, true);
 	}
 	
 	public function size($queue)
 	{
-		return $this->_redis->llen('queue:' . $queue);
+		
 	}
 	
 	public function enqueue($queue, $class, $args = null)
 	{
-		$id = md5(uniqid('', true));
-		
-		$result = Job::create($queue, $class, $args);
-		if($result)
-		{
-			\Event::trigger('queue.after_enqueue', array(
-				'class' => $class,
-				'args' => $args,
-			));
-		}
-		
-		return $result;
+	
 	}
 	
 	public function reserve($queue)
 	{
-		return Job::reserve($queue);
+	
 	}
 	
 	public function queues()
 	{
-		$queues = $this->_redis->smembers('queues');
-		if(!is_array($queues))
-		{
-			$queues = array();
-		}
-		return $queues;
+	
 	}
 }
