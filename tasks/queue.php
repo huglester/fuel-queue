aeae3a8f2f3595f7395f990bd760ed4a807ae10d
Matt Lindesay <matt@lindesay.com>
2012-10-11 08:35:37 +0100

    fixed bug for > 25 queue items

--- tasks/queue.php
+++ tasks/queue.php
@@ -19,63 +19,65 @@ class Queue
 		{
 			$queue = 'default';
 		}
-				
-		\Cli::write('[INFO] Processing queue: '.$queue);		
-		
+
+		\Cli::write('[INFO] Processing queue: '.$queue);
+
 		try {
-			
+
 			$run_number = 0;
 			while(true)
 			{
 				sleep(5);
 				\Cli::write('[INFO] Starting run');
-				
+
 				$queue_size = \Queue::size($queue);
-				
+
 				if($queue_size == 0)
 				{
 					continue;
 				}
-				
-				if ($queue_size < 25)
-				{
-					$runs = $queue_size;
-				}
-				
+
+				// if ($queue_size < 25)
+				// {
+				// 	$runs = $queue_size;
+				// }
+
+				$runs = $queue_size < 25 ? $queue_size : 25;
+
 				\Cli::write('[INFO] Queue size: '.$queue_size);
-				$total_usage = 0;		
+				$total_usage = 0;
 				for($i = 0; $i < $runs; ++$i)
-				{				
+				{
 					// execution environment
 					$job = \Queue::pop($queue);
-					\Cli::write('[INFO] -------------- RUNNING JOB: '.$job['class'].' args:'.@implode(',',$job['args']));		
+					\Cli::write('[INFO] -------------- RUNNING JOB: '.$job['class'].' args:'.@implode(',',$job['args']));
 					\Oil\Refine::run($job['class'], $job['args']);
 					$total_usage += memory_get_usage();
 					\Cli::write('[INFO] -------------- END JOB: memory usage: '.\Num::format_bytes(memory_get_usage(), 2).' elapsed time:');
 				}
-				
-				\Cli::write("[INFO] Performed $i jobs");	
+
+				\Cli::write("[INFO] Performed $i jobs");
 				if($i > 0)
 				{
 					\Cli::write("[INFO] Total memory usage in this batch: ".\Num::format_bytes($total_usage, 2));
 					\Cli::write("[INFO] ".\Queue::size($queue)." jobs lefts on the queue $queue");
 				}
-							
-				
+
+
 				$run_number++;
 			}
-			
+
 		}
 		catch(Exception $e)
 		{
 			\Cli::color("[ERROR] Error: ". $e->getMessage(), 'red');
 		}
-		
-		
+
+
 		\Cli::write('Total executions: '.$i);
 		\Cli::write('Execution completed!');
 	}
-	
+
 	public static function help()
 	{
 		\Cli::write('Usage: php oil r|refine [<queue_name>]');
